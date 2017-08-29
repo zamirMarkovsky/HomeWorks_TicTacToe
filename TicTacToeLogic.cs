a6ff5f388b5e28b4cc063fb9f2af7c421e4e61f3
zamirMarkovsky <16.zamir@gmail.com>
2017-08-29 20:34:30 +0300

    Add files via upload

--- TicTacToeLogic.cs
+++ TicTacToeLogic.cs
@@ -18,25 +18,25 @@ namespace C17_Ex02
         }
 
         //The method tries to insert to symbol to the desired place and returns if successful
-        public static bool TryInsert(char symbol, ref char[,] io_table, int rowInput, int colInput)
+        public static bool TryInsert(char symbol, ref char[,] io_table, int i_rowInput, int i_colInput)
         {
             bool isChanged = false;
 
-            if (io_table[rowInput, colInput] == ' ')
+            if (io_table[i_rowInput, i_colInput] == ' ')
             {
-                io_table[rowInput, colInput] = symbol;
+                io_table[i_rowInput, i_colInput] = symbol;
                 isChanged = true;
             }
             return isChanged;
         }
 
         //The method checks all the possible options for a streight and returns if successful
-        public static bool checkforStraight(char[,] io_table, char symbol, int numOfSteps)
+        public static bool checkforStraight(char[,] io_table, char i_symbol, int i_numOfSteps)
         {
             int edgeSize = io_table.GetLength(0);
             bool isStraight = false;
 
-            if (numOfSteps >= (edgeSize * 2 - 2))
+            if (i_numOfSteps >= (edgeSize * 2 - 2))
             {
                 int matchesCount;
 
@@ -46,7 +46,7 @@ namespace C17_Ex02
                     matchesCount = 0;
                     for (int colIndex = 0; colIndex < edgeSize; colIndex++)
                     {
-                        if (io_table[rowIndex, colIndex] == symbol)
+                        if (io_table[rowIndex, colIndex] == i_symbol)
                         {
                             matchesCount++;
                         }
@@ -59,7 +59,7 @@ namespace C17_Ex02
                     matchesCount = 0;
                     for (int rowIndex = 0; rowIndex < edgeSize; rowIndex++)
                     {
-                        if (io_table[rowIndex, colIndex] == symbol)
+                        if (io_table[rowIndex, colIndex] == i_symbol)
                         {
                             matchesCount++;
                         }
@@ -70,7 +70,7 @@ namespace C17_Ex02
                 matchesCount = 0;
                 for (int rowAndColIndex = 0; rowAndColIndex < edgeSize && !isStraight; rowAndColIndex++)
                 {
-                    if (io_table[rowAndColIndex, rowAndColIndex] == symbol)
+                    if (io_table[rowAndColIndex, rowAndColIndex] == i_symbol)
                     {
                         matchesCount++;
                     }
@@ -81,7 +81,7 @@ namespace C17_Ex02
                 for (int rowIndex = edgeSize - 1; rowIndex >= 0 && !isStraight; rowIndex--)
                 {
                     int colIndex = (edgeSize - 1) - rowIndex;
-                    if (io_table[rowIndex, colIndex] == symbol)
+                    if (io_table[rowIndex, colIndex] == i_symbol)
                     {
                         matchesCount++;
                     }
@@ -93,9 +93,9 @@ namespace C17_Ex02
         }
 
         //The method declairs the winner
-        public static string findWinner(ref PlayerDetails io_player1, ref PlayerDetails io_player2, char symbol)
+        public static string findWinner(ref PlayerDetails io_player1, ref PlayerDetails io_player2, char i_symbol)
         {
-            if (io_player1.m_PlayerSymbol == symbol)
+            if (io_player1.m_PlayerSymbol == i_symbol)
             {
                 io_player2.m_Points++;
                 return io_player2.m_Name;
